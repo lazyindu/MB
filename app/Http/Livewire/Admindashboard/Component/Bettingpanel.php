d94fd6e9ef2b534c2833066894b8bb93410d1b3e
Lazyindu <chhotasalman05@gmail.com>
2023-05-17 11:43:08 +0530

    new

---
+++ app/Http/Livewire/Admindashboard/Component/Bettingpanel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admindashboard\Component;
+
+use Livewire\Component;
+
+class Bettingpanel extends Component
+{
+    public function render()
+    {
+        return view('livewire.admindashboard.component.bettingpanel');
+    }
+}
---
+++ resources/views/livewire/admindashboard/component/base.blade.php
@@ -0,0 +1,9 @@
+<div>
+    @livewire('admindashboard.component.header')
+
+    <div class="d-flex align-items-stretch">
+        <!-- Sidebar Navigation-->
+        @livewire('admindashboard.component.navbar')
+        @livewire('admindashboard.component.bettingpanel')
+    </div>
+</div>
